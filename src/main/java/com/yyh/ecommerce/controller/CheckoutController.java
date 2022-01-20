b6a5f33cc8b333cd260969b7c7f2d8f707436f4a
yyhyap <yongheng98@hotmail.com>
2022-01-20 21:08:38 +0800

    Added Stripe for payment processing

--- src/main/java/com/yyh/ecommerce/controller/CheckoutController.java
+++ src/main/java/com/yyh/ecommerce/controller/CheckoutController.java
@@ -6,6 +6,7 @@ import com.yyh.ecommerce.dto.PaymentInfo;
 import com.yyh.ecommerce.dto.Purchase;
 import com.yyh.ecommerce.dto.PurchaseResponse;
 import com.yyh.ecommerce.service.CheckoutService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
+@Slf4j
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
@@ -30,6 +32,9 @@ public class CheckoutController {
 
     @PostMapping("/payment-intent")
     public ResponseEntity<String> createPaymentIntent(@Valid @RequestBody PaymentInfo paymentInfo) throws StripeException {
+
+        log.info("Payment amount: {}", paymentInfo.getAmount());
+        log.info("Payment currency: {}", paymentInfo.getCurrency());
         PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
         String paymentString = paymentIntent.toJson();
 
